4e5a0a0b0b4840248ff0f691c239f9da2daf0f58
RAUL BORGES RODRIGUES <raul.brodrigues@senacsp.edu.br>
2025-05-05 09:44:23 -0300

    Cadastrar Turma em Andamento

---
+++ BJJGerenciamento.UI/CadastrarTurmas.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CadastrarTurmas.aspx.cs" Inherits="BJJGerenciamento.UI.CadastrarTurmas" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Cadastrar Turma</h2>
+
+    <asp:Label ID="lblPlano" runat="server" Text="Escolha o Plano: " />
+    <asp:DropDownList ID="ddlPlano" runat="server">
+    </asp:DropDownList>
+    <br />
+
+    <asp:Label ID="lblDias" runat="server" Text="Escolha os Dias: " />
+    <asp:CheckBoxList ID="cblDias" runat="server">
+    </asp:CheckBoxList>
+    <br />
+
+    <asp:Label ID="lblHorarios" runat="server" Text="Escolha os Horários: " />
+    <asp:PlaceHolder ID="phHorarios" runat="server">
+
+    </asp:PlaceHolder>
+    <br />
+
+    <asp:Label ID="lblMensalidade" runat="server" Text="Mensalidade: " />
+    <asp:TextBox ID="txtMensalidade" runat="server" ReadOnly="true" />
+    <br />
+
+    <asp:Button ID="btnSalvar" runat="server" Text="Salvar" OnClick="btnSalvar_Click" />
+</asp:Content>
---
+++ BJJGerenciamento.UI/CadastrarTurmas.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace BJJGerenciamento.UI
+{
+
+
+    public partial class CadastrarTurmas
+    {
+
+        /// <summary>
+        /// Controle lblPlano.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPlano;
+
+        /// <summary>
+        /// Controle ddlPlano.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlPlano;
+
+        /// <summary>
+        /// Controle lblDias.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDias;
+
+        /// <summary>
+        /// Controle cblDias.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBoxList cblDias;
+
+        /// <summary>
+        /// Controle lblHorarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblHorarios;
+
+        /// <summary>
+        /// Controle phHorarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder phHorarios;
+
+        /// <summary>
+        /// Controle lblMensalidade.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensalidade;
+
+        /// <summary>
+        /// Controle txtMensalidade.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMensalidade;
+
+        /// <summary>
+        /// Controle btnSalvar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSalvar;
+    }
+}
