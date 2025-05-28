b8fe61d6d90bf9f56a1837627a3d611eed2d226a
marianetelesbento <marianebteles2021@hotmail.com>
2025-05-28 11:08:38 -0300

    .

--- BJJGerenciamento.UI/Home.aspx.cs
+++ BJJGerenciamento.UI/Home.aspx.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using BJJGerenciamento.UI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BJJGerenciamento.UI.DAL;
 
 namespace BJJGerenciamento.UI
 {
@@ -16,11 +18,19 @@ namespace BJJGerenciamento.UI
                 Response.Redirect("Login.aspx");
             }
 
+            if (!IsPostBack)
+            {
+                int ativos = AlunosDAL.ObterQuantidadeAtivos();
+                int inativos = AlunosDAL.ObterQuantidadeInativos();
+                List<AlunoModels> aniversariantes = AlunosDAL.ObterAniversariantesDoMes();
+
+                lblAtivos.Text = ativos.ToString();
+                lblInativos.Text = inativos.ToString();
+                rptAniversariantes.DataSource = aniversariantes;
+                rptAniversariantes.DataBind();
+            }
+
         }
-        //protected void btnSair_Click(object sender, EventArgs e)
-        //{
-        //    Session.Clear();
-        //    Response.Redirect("Login.aspx");
-        //}
+        
     }
 }
--- BJJGerenciamento.UI/Home.aspx.designer.cs
+++ BJJGerenciamento.UI/Home.aspx.designer.cs
@@ -13,5 +13,32 @@ namespace BJJGerenciamento.UI
 
     public partial class Home
     {
+
+        /// <summary>
+        /// Controle lblAtivos.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAtivos;
+
+        /// <summary>
+        /// Controle lblInativos.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblInativos;
+
+        /// <summary>
+        /// Controle rptAniversariantes.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptAniversariantes;
     }
 }
